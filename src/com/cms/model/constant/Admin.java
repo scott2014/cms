5ae6adc02bfba3332ec86d98a75cd373e72d8a8d
scott smith <626306805@qq.com>
2014-04-30 17:29:53 +0800

    管理员账户常量类

---
+++ src/com/cms/model/constant/Admin.java
@@ -0,0 +1,7 @@
+package com.cms.model.constant;
+
+public interface Admin {
+	public static final String ACCOUNT1 = "admin";
+	public static final String ACCOUNT2 = "admin2";
+	public static final String ACCOUNT3 = "admin3";
+}
