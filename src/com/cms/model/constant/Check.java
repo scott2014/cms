9c45604d84eca5dd70ea93aed741f74ad5a63c92
scott smith <626306805@qq.com>
2014-04-30 18:14:32 +0800

    审核状态常量类

---
+++ src/com/cms/model/constant/Check.java
@@ -0,0 +1,11 @@
+package com.cms.model.constant;
+
+public interface Check {
+	
+	//审核通过
+	public static final int APPROVED = 0x1;
+	//审核拒绝
+	public static final int REFUSED = 0x2;
+	//未审批
+	public static final int NOT_APPROVE = 0x3;
+}
