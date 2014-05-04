116f38caf76b77aa5463a4704768efd637ec8134
scott2014 <626306805@qq.com>
2014-05-04 08:19:09 +0800

    数据库文件更新

--- sql/cms.sql
+++ sql/cms.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
+Source Server         : mysql
 Source Server Version : 50536
 Source Host           : localhost:3306
 Source Database       : cms
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50536
 File Encoding         : 65001
 
-Date: 2014-04-30 18:15:07
+Date: 2014-05-04 08:18:19
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -73,14 +73,15 @@ CREATE TABLE `medicinal` (
   `description` varchar(255) DEFAULT NULL,
   `userId` bigint(64) DEFAULT NULL,
   `createTime` datetime DEFAULT NULL,
-  `left` int(32) DEFAULT NULL,
+  `_left` int(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of medicinal
 -- ----------------------------
-INSERT INTO `medicinal` VALUES ('1', 'CH4', '甲烷', 'CC01', '无色、无味', null, '远离易燃物', '0.53kg/包', '1', '/upload/1398573314780.gif', 'm01', '低温存放', '1', null, null);
+INSERT INTO `medicinal` VALUES ('1', '', '', '', '', '', '', '', '1', '/upload/1398878467809.gif', '', '', '1', '2014-05-01 01:21:07', null);
+INSERT INTO `medicinal` VALUES ('2', '', '', '', '', null, '', '', '1', '/upload/1398917162646.gif', '', '', '1', '2014-05-01 12:06:02', null);
 
 -- ----------------------------
 -- Table structure for repository
@@ -94,15 +95,17 @@ CREATE TABLE `repository` (
   `userId` bigint(64) DEFAULT NULL,
   `repoName` varchar(64) DEFAULT NULL,
   `createTime` datetime DEFAULT NULL,
+  `university` varchar(255) DEFAULT NULL,
+  `faculty` varchar(255) DEFAULT NULL,
+  `team` varchar(255) DEFAULT NULL,
+  `fields` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of repository
 -- ----------------------------
-INSERT INTO `repository` VALUES ('1', 'A01', '西北大学化学系', '西北大学', '1', '西北大学化学实验室', null);
-INSERT INTO `repository` VALUES ('2', 'A02', '西北大学物理系', '西北大学', '1', '西北大学物理实验室', null);
-INSERT INTO `repository` VALUES ('3', 'dssc1', 'FUDAN', '713药品柜', '4', '713', null);
+INSERT INTO `repository` VALUES ('1', 'a01', '化学实验室', '西北大学老校区', '1', '我的试剂库', null, null, null, null, null);
 
 -- ----------------------------
 -- Table structure for user
@@ -116,22 +119,29 @@ CREATE TABLE `user` (
   `QQ` varchar(16) DEFAULT NULL,
   `realName` varchar(16) DEFAULT NULL,
   `phone` varchar(16) DEFAULT NULL,
+  `status` int(4) DEFAULT NULL,
+  `gender` int(4) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `university` varchar(255) DEFAULT NULL,
+  `department` varchar(255) DEFAULT NULL,
+  `faculty` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'admin', '21232f297a57a5a743894a0e4a801fc3', '809113633@qq.com', '626306805', null, '13572459570');
-INSERT INTO `user` VALUES ('2', 'scott', '50f1725279347cd1e04bf016026ae1b9', null, null, null, null);
-INSERT INTO `user` VALUES ('3', 'we', 'ff1ccf57e98c817df1efcd9fe44a8aeb', null, null, null, null);
-INSERT INTO `user` VALUES ('4', 'lam713', 'e10adc3949ba59abbe56e057f20f883e', 'ydy3128@126.com', '821836717', null, '18721323885');
-INSERT INTO `user` VALUES ('5', 'abc', '900150983cd24fb0d6963f7d28e17f72', null, null, null, null);
-INSERT INTO `user` VALUES ('6', 'nayangnianhua', '50f1725279347cd1e04bf016026ae1b9', null, null, null, null);
-INSERT INTO `user` VALUES ('7', '李王娜', '202cb962ac59075b964b07152d234b70', null, null, null, null);
-INSERT INTO `user` VALUES ('8', 'xuesheng', '81dc9bdb52d04dc20036dbd8313ed055', null, null, null, null);
-INSERT INTO `user` VALUES ('9', 'admin2010', '81dc9bdb52d04dc20036dbd8313ed055', null, null, null, null);
-INSERT INTO `user` VALUES ('10', 'a2', '0cc175b9c0f1b6a831c399e269772661', null, null, null, null);
+INSERT INTO `user` VALUES ('1', null, null, '809113633@qq.com', '626306805', 'aa', '13572459570', null, null, 'aa', 'aaaaaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaa', '/upload/1399130701944.gif');
+INSERT INTO `user` VALUES ('2', 'scott', '50f1725279347cd1e04bf016026ae1b9', null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('3', 'we', 'ff1ccf57e98c817df1efcd9fe44a8aeb', null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('4', 'lam713', 'e10adc3949ba59abbe56e057f20f883e', 'ydy3128@126.com', '821836717', null, '18721323885', null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('5', 'abc', '900150983cd24fb0d6963f7d28e17f72', 'a', 'a', 'a', 'a', null, null, 'aa', 'aaaa', 'aa', 'aa', 'aa', '/upload/1399131290920.gif');
+INSERT INTO `user` VALUES ('6', 'nayangnianhua', '50f1725279347cd1e04bf016026ae1b9', null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('7', 'admin2', '202cb962ac59075b964b07152d234b70', null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('8', 'xuesheng', '202cb962ac59075b964b07152d234b70', null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('9', 'admin', '21232f297a57a5a743894a0e4a801fc3', null, null, null, null, null, null, null, null, null, null, null, null);
 
 -- ----------------------------
 -- Table structure for user_medicinal
@@ -143,12 +153,19 @@ CREATE TABLE `user_medicinal` (
   `medicinalId` bigint(64) DEFAULT NULL,
   `status` int(4) DEFAULT NULL,
   `checkTime` datetime DEFAULT NULL,
+  `applyTime` datetime DEFAULT NULL,
+  `applyNumber` float(10,0) DEFAULT NULL,
+  `appId` bigint(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_medicinal
 -- ----------------------------
+INSERT INTO `user_medicinal` VALUES ('2', '1', '1', '3', null, '2014-05-01 08:20:15', '12', null);
+INSERT INTO `user_medicinal` VALUES ('3', '9', '1', '2', null, '2014-05-01 11:13:32', '13', '1');
+INSERT INTO `user_medicinal` VALUES ('4', '1', '2', '1', null, '2014-05-01 12:07:43', '0', '1');
+INSERT INTO `user_medicinal` VALUES ('5', '5', '1', '2', null, '2014-05-01 22:55:00', '10', '1');
 
 -- ----------------------------
 -- Table structure for user_right
@@ -163,12 +180,14 @@ CREATE TABLE `user_right` (
   KEY `rightId` (`rightId`),
   CONSTRAINT `user_right_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`),
   CONSTRAINT `user_right_ibfk_2` FOREIGN KEY (`rightId`) REFERENCES `_right` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_right
 -- ----------------------------
-INSERT INTO `user_right` VALUES ('1', '10', '1');
+INSERT INTO `user_right` VALUES ('2', '7', '1');
+INSERT INTO `user_right` VALUES ('3', '8', '1');
+INSERT INTO `user_right` VALUES ('4', '9', '2');
 
 -- ----------------------------
 -- Table structure for _right
@@ -184,5 +203,5 @@ CREATE TABLE `_right` (
 -- ----------------------------
 -- Records of _right
 -- ----------------------------
-INSERT INTO `_right` VALUES ('1', '1', '学生');
-INSERT INTO `_right` VALUES ('2', '2', '管理员');
+INSERT INTO `_right` VALUES ('1', '1', '管理员');
+INSERT INTO `_right` VALUES ('2', '2', '学生');
